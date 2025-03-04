023d2ab0882eb3a4761dad2ea64a256d1edf78ca
Fayella <133595647+OCEANiNFERNO@users.noreply.github.com>
2025-03-04 01:15:39 +0000

    details added

--- main.py
+++ main.py
@@ -1,42 +1,108 @@
+import random
 
+order_review = True
 loop = True
 # Welcome Message
-print("Welcome to Minerva's Shop!")
+print("Thanks for contacting customer service.")
 
 #Name and Age Variables
 customer = input("What's your name? ")
-cAge = input(f"Nice to meet you {customer}, How old are you?:  ")
-if int(cAge) < 18:
-    print("Wow, you're a little bit young to be shopping here... but that's alright!")
+print(f"It seems like you're having trouble with your order {customer}, ")
+order_number = input("Please input your order number: ")
 
 
 #the menu
 def menu():
-    print("\n\n\n=== This is Minerva's Shop === ")
-    print("1. Head over to food and drink section.")
-    print("2. Head over to the clothes section. ")
-    print("3. Come and checkout!")
-    print("4. Exit the shop.")
+    print("\n\n=== How Can I Help You?  === ")
+    print("1. Track Order")
+    print("2. Reorder ")
+    print("3. Refund Order")
+    print("4. Exit.")
     print("       =================\n")
 
 
 #the interface
 def interface():
-    shopping = True
+    service = True
     menu()
     menu_selection = input("Where would you like to go?(1-4): ")
     
-    if int(menu_selection) == 1:
-        print("Option isn't implemented yet.")
+    #Tracking Order
+    if int(menu_selection) == 1:    
+        track_order()
+    #Reordering
     elif int(menu_selection) == 2:
-        print("Option isn't implemented yet.")
+        reorder()
+    #Refunding Order
     elif int(menu_selection) == 3:
-        print("Option isn't implemented yet.")
+        refund()
     elif int(menu_selection) == 4:
-        print(f"Thanks for shopping {customer}. Hope you enjoy your day!")
-        shopping = False
-    return shopping
+        print(f"Thanks for contacting service, {customer}. Hope you enjoy your day!")
+        service = False
+    return service
                       
+
+
+#Random Minutes
+mins = random.randint(1,300)
+#Universal Order Status ===========
+status = [f"Your order will arive in {mins} minutes.", 
+                f"Your order #{order_number} has been lost.", 
+                "It appears our driver ate your order.", 
+                f"Your order #{order_number} is being prepared"]
+#Choosing from list
+order_status = random.choice(status)
+# ==================================
+
+
+#OPTION 1
+def track_order():
+    print(f"\n{order_status}")
+    if order_status == f"Your order #{order_number} has been lost." or "It appears our driver ate your order.":
+        review()
+
+
+
+#OPTION 2
+def reorder():
+    confirm = input("Would you like to re-order your order? (Y/N):")
+    if confirm == "Y":
+        if order_status == f"Your order will arive in {mins} minutes." or f"Your order #{order_number} is being prepared":
+            confirm2 = input("Your order will be arriving soon, are you sure you want to reorder? (Y/N): ")
+            if confirm2 == "N":
+                return
+        print("Successfully re-ordered your order.")
+    
+
+#OPTION 3
+#Check the user's order status to see if they're eligible for a refund.
+def refund():
+        if order_status == f"Your order #{order_number} has been lost." or "It appears our driver ate your order.":
+            print(order_status)
+            print("Sorry for the unfortunate experience. It seems like you're eligble for a refund.")
+            print("Your money will be transferred back in the next 1-5 business days.")
+            review()
+        else: 
+            print("Sorry, it doesn't seem like you're eligible for a review.")
+
+
+# Universal Review
+def review():
+    while order_review:
+        complain = input("Would you like to file a complaint? (Y/N): ")
+        if complain == "Y":
+            stars = input("Rate your order 1-5 stars: ")
+            print(f"You've rated your order {stars} stars.\n") 
+            feedback = input("Write any additional feedback you have, or N/A if none: ")
+            print("Thanks for the feedback!")
+            break       
+        elif complain == "N":
+            break
+        else:
+            print("Sorry this is not a valid input.")
+
+
+
 # This double loop method was from C2C
 while loop:
     interface()
